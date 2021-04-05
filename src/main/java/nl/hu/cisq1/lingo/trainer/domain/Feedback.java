b2d728f7a158868e834138598311d07298c72e01
Erik <erik.meijer@student.hu.nl>
2021-04-05 03:54:35 +0200

    added hints + tests

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -2,6 +2,7 @@ package nl.hu.cisq1.lingo.trainer.domain;
 
 import nl.hu.cisq1.lingo.trainer.domain.enums.Mark;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -15,11 +16,27 @@ public class Feedback {
     }
 
     public boolean isWordGuessed(){
-        return marks.stream().allMatch(mark -> mark == Mark.CORRECT);
+        return marks.stream().allMatch(letter -> letter == Mark.CORRECT);
     }
 
     public boolean isGuessInvalid(){
         return marks.contains(Mark.INVALID);
+    }
+
+    public List<Character> giveHint(List<Character> previousHint, String answer){
+        List<Character> hint = new ArrayList<Character>();
+        for (int i = 0; i < answer.length(); i++){
+            if(marks.get(i) == Mark.CORRECT){
+                hint.add(attempt.charAt(i));
+            }
+            else if(previousHint.get(i) != '.'){
+                hint.add(previousHint.get(i));
+            }
+            else{
+                hint.add('.');
+            }
+        }
+        return hint;
     }
 
     @Override
--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -3,8 +3,14 @@ package nl.hu.cisq1.lingo.trainer.domain;
 import nl.hu.cisq1.lingo.trainer.domain.enums.Mark;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,22 +19,22 @@ class FeedbackTest {
     @Test
     @DisplayName("word is guessed if all letters are correct")
     void wordIsGuessed(){
-        //Arrange (given)
+        //given
 
-        //Act (when)
+        //when
         Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
-        //Assert (then)
+        //then
         assertTrue(feedback.isWordGuessed());
     }
 
     @Test
     @DisplayName("word not is guessed if not all letters are correct")
     void wordIsNotGuessed(){
-        //Arrange (given)
+        //given
 
-        //Act (when)
+        //when
         Feedback feedback = new Feedback("woord", List.of(Mark.ABSENT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
-        //Assert (then)
+        //then
         assertFalse(feedback.isWordGuessed());
     }
 
@@ -36,22 +42,54 @@ class FeedbackTest {
     @Test
     @DisplayName("guess is invalid if word does not exist or word is not the right amount of letters")
     void guessIsInvalid(){
-        //Arrange (given)
+        //given
 
-        //Act (when)
+        //when
         Feedback feedback = new Feedback("woord", List.of(Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID));
-        //Assert (then)
+        //then
         assertTrue(feedback.isGuessInvalid());
     }
 
     @Test
     @DisplayName("guess is not invalid if the guessed word exists and is of right length")
     void guessIsNotInvalid(){
-        //Arrange (given)
+        //given
 
-        //Act (when)
+        //when
         Feedback feedback = new Feedback("woord", List.of(Mark.ABSENT, Mark.CORRECT, Mark.PRESENT, Mark.CORRECT, Mark.CORRECT));
-        //Assert (then)
+        //then
         assertFalse(feedback.isGuessInvalid());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideHintExamples")
+    @DisplayName("hint is correct if the characters and points are in the correct position according to the attempt and the final word")
+    void hintIsCorrect(List<Character> previousHint, Feedback feedback, String correctAnswer, List<Character> desiredHint){
+        assertEquals(desiredHint, feedback.giveHint(previousHint, correctAnswer));
+    }
+
+    private static Stream<Arguments> provideHintExamples() {
+
+        String correctAnswer = "pasta";
+        List<Character> previousHint = Arrays.asList('.', 'a', '.', '.', '.');
+
+
+        Feedback invalidFeedback = new Feedback("paasen", List.of(Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID));
+        //should return:
+        List<Character> invalidHint = Arrays.asList('.', 'a', '.', '.', '.');
+
+        Feedback presentFeedback = new Feedback("paard", List.of(Mark.CORRECT, Mark.CORRECT, Mark.PRESENT, Mark.ABSENT, Mark.ABSENT));
+        //should return:
+        List<Character> presentHint = Arrays.asList('p', 'a', '.', '.', '.');
+
+        Feedback correctFeedback = new Feedback("pasta", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        //should return:
+        List<Character> correctHint = Arrays.asList('p', 'a', 's', 't', 'a');
+
+        return Stream.of(
+                Arguments.of(previousHint, invalidFeedback, correctAnswer, invalidHint),
+                Arguments.of(previousHint, presentFeedback, correctAnswer, presentHint),
+                Arguments.of(previousHint, correctFeedback, correctAnswer, correctHint)
+        );
     }
 }
