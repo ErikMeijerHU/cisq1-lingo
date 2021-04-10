0501440ebb3658fa55a749e66e95943ab0c77549
Erik <erik.meijer@student.hu.nl>
2021-04-10 21:48:40 +0200

    test fix 2

--- src/test/java/nl/hu/cisq1/lingo/trainer/application/GameServiceIntegrationTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/application/GameServiceIntegrationTest.java
@@ -6,20 +6,24 @@ import nl.hu.cisq1.lingo.trainer.data.SpringGameRepository;
 import nl.hu.cisq1.lingo.trainer.domain.Game;
 import nl.hu.cisq1.lingo.trainer.presentation.dto.GameDTO;
 import nl.hu.cisq1.lingo.words.data.SpringWordRepository;
+import nl.hu.cisq1.lingo.words.domain.Word;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Import(CiTestConfiguration.class)
@@ -28,7 +32,7 @@ public class GameServiceIntegrationTest {
     @Autowired
     private SpringGameRepository gameRepository;
 
-    @Autowired
+    @MockBean
     private SpringWordRepository wordRepository;
 
     @Autowired
@@ -56,10 +60,15 @@ public class GameServiceIntegrationTest {
     @DisplayName("guessing a word correctly should increase score correctly")
     void guessingAWord(List<String> guesses, int expectedScore){
         GameDTO gameDTO = null;
+        when(this.wordRepository.findWordByValue("steen")).thenReturn(Optional.of(new Word("steen")));
+        when(this.wordRepository.findWordByValue("staal")).thenReturn(Optional.of(new Word("staal")));
+        when(this.wordRepository.findWordByValue("stoel")).thenReturn(Optional.of(new Word("stoel")));
+
         for(String guess : guesses) {
             gameDTO = this.service.guess(game.getId(), guess);
         }
-       assertEquals(expectedScore, gameDTO.score);
+
+        assertEquals(expectedScore, gameDTO.score);
     }
 
     private static Stream<Arguments> provideGuesses() {
